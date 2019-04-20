e61e7aa3d5d9f2800729cf45982db8c1e88b4bf7
Moe Kyaw Soe <31211920+moekyawsoe@users.noreply.github.com>
2019-04-20 13:17:00 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Download Zip
+2. Import in your project
+3. Copy php codes in your project
---
+++ zawgyi-myanmar3.php
@@ -0,0 +1,13 @@
+<html xmlns="http://www.w3.org/1999/xhtml" >
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Example for using Zawgyi To Myanmar3 Converter Web Service</title>
+	</head>
+	<body>
+		<?php
+			require_once('lib.php');
+			$converted_string= ZawgyiToMyanmar3('');
+			echo $converted_string;
+		?>
+	</body>
+</html>
